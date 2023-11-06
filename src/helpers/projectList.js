b20d997f282987799235d3c4b8168f4a99f33705
Artyom Volkov <artemvolk2099@yandex.ru>
2023-11-06 18:41:45 +0300

    Added new project

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -1,19 +1,22 @@
-import rssSmall from '../img/projects/rss.png';
+// import rssSmall from '../img/projects/rss.png';
 import rssBig from '../img/projects/rss.png';
 import chatBig from '../img/projects/chat-big.png';
-import chatAppSm from '../img/projects/chatApp-sm.png'
-import gendiffSmall from '../img/projects/gendiff-small.png';
+// import chatAppSm from '../img/projects/chatApp-sm.png'
+// import gendiffSmall from '../img/projects/gendiff-small.png';
 import gendiffBig from '../img/projects/gendiff.png';
-import brainSmall from '../img/projects/brain-games-small.png';
+// import brainSmall from '../img/projects/brain-games-small.png';
 import brainBig from '../img/projects/brain-games.png';
 import gitHubBlack from '../img/icons/gitHub-black.svg';
-import artagentSm from '../img/projects/artagent-small.png';
+// import artagentSm from '../img/projects/artagent-small.png';
 import architecturaBig from '../img/projects/architectural-big.png';
+import lookShareBig from '../img/projects/lookShare-big.png';
+import lookShareSm from '../img/projects/lookShare-sm.png';
+
 
 const projects = [
     {
         title: 'Rss агрегатор' ,
-        img: rssSmall,
+        img: rssBig,
         imgBig: rssBig,
         githubLink: 'https://github.com/ArtyomVolkov1/frontend-project-11',
         deployLink: 'https://frontend-project-11-smoky-six.vercel.app/',
@@ -26,7 +29,7 @@ const projects = [
     },
     {
         title: 'Chat App' ,
-        img: chatAppSm,
+        img: chatBig,
         imgBig: chatBig,
         githubLink: 'https://github.com/ArtyomVolkov1/frontend-project-12',
         deployLink: 'https://frontend-project-12-qyp1.onrender.com/',
@@ -39,8 +42,21 @@ const projects = [
         key: 2,
     },
     {
+        title: 'Look&Share' ,
+        img: lookShareSm,
+        imgBig: lookShareBig,
+        githubLink: 'https://github.com/ArtyomVolkov1/fullstack_LookShareApp',
+        deployLink: 'https://fullstack-look-share-app.vercel.app/sign-in',
+        buttonImg: gitHubBlack,
+        previewName: 'Preview',
+        buttonName: 'Github repo',
+        description: 'Look&Share - упрощенное приложение для обмена фотографиями. В L&S есть возможность добавлять, редактировать, ставить лайки и добавлять в избранное.',
+        skills: ' React/Typescript, React-router-dom, TailwindCSS, shadcn ui, TanStack Query v5, Appwrite ',
+        key: 3,
+    },
+    {
         title: 'Landing' ,
-        img: artagentSm,
+        img: architecturaBig,
         imgBig: architecturaBig, 
         githubLink: 'https://github.com/ArtyomVolkov1/architectural-agency',
         deployLink: 'https://artyomvolkov1.github.io/architectural-agency/',
@@ -49,29 +65,29 @@ const projects = [
         buttonName: 'Github repo',
         description: 'Лендинг архитектурного агентства с формой обратной связи (демо-версия)',
         skills: 'HTML, SCSS, БЭМ, адаптивная верстка',
-        key: 3,
+        key: 4,
     },
     {
         title: 'Brain-Games' ,
-        img: brainSmall,
+        img: brainBig,
         imgBig: brainBig, 
         githubLink: 'https://github.com/ArtyomVolkov1/frontend-project-44',
         buttonImg: gitHubBlack,
         buttonName: 'Github repo',
         description: 'Набор из пяти простых консольных игр. Каждая игра задает вопросы, на которые нужно дать правильные ответы',
         skills: 'Js, Lodash, Функциональное программирование',
-        key: 4,
+        key: 5,
     },
     {
         title: 'Difference Calculator' ,
-        img: gendiffSmall,
+        img: gendiffBig,
         imgBig: gendiffBig, 
         githubLink: 'https://github.com/ArtyomVolkov1/frontend-project-46',
         buttonImg: gitHubBlack,
         buttonName: 'Github repo',
         description: 'Программа, определяющая разницу между двумя структурами данных.(JSON или YML формате)',
         skills: 'Js, Jest, Commander.js, Построение архитектуры проекта',
-        key: 5,
+        key: 6,
     },
 ];
 
